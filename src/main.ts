f8d3c02d9106af6d456369d563f7115851531e02
Afaan Bilal <afaanbilal@gmail.com>
2023-02-26 18:00:58 +0530

    ✨ Multiple monitors

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,11 @@ const h1 = document.createElement("h1");
 createMonitor(() => h1.innerText = `Count is ${count()}`);
 
 const btn = document.createElement("button");
-btn.innerText = "Increment";
+createMonitor(() => btn.innerText = `Increment (To: ${count() + 1})`);
 btn.onclick = () => setCount(count() + 1);
 
 const btnDouble = document.createElement("button");
-btnDouble.innerText = "Double";
+createMonitor(() => btnDouble.innerText = `Double (To: ${count() * 2})`);
 btnDouble.onclick = () => setCount(count() * 2);
 
 document.body.append(h1, btn, btnDouble);
